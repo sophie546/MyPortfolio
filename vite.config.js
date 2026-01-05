6fb598db3e954a0e4f83a023fcda73c901338746
seobi <sophie.aloria@gmail.com>
2026-01-06 00:01:26 +0800

    Initial commit - my portfolio React + Vite

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ export default defineConfig({
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
+  preview: {
+    allowedHosts: ['myportfolio-jsbz.onrender.com'],
+    port: 4173,     
+  }
 })
